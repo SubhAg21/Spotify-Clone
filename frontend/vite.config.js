1dc40c8376c3e755952d0b90e8b1c6c7917c66e1
SubhAg21 <subhagarwal3716@gmail.com>
2025-11-03 10:10:52 +0530

    Update API proxy target to production URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:"http://localhost:5000",
+        target:"https://spotify-clone-backend-as6g.onrender.com",
       }
     }
   }
